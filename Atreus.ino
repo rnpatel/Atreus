a341110f17d9e0659e2b892e3a9439fe7195f0a0
Rajiv Patel <rpatel@voalte.com>
2021-01-19 13:23:30 -0500

    home row modifiers
    - moved the modifiers
    - fixed number layer switch

--- Atreus.ino
+++ Atreus.ino
@@ -30,6 +30,7 @@
 
 #define MO(n) ShiftToLayer(n)
 #define TG(n) LockLayer(n)
+#define L(n) MoveToLayer(n)
 
 #define KEYCHORD_HYPER LSHIFT(LALT(LGUI(Key_LeftControl)))
 #define KEYCHORD_MEH   LSHIFT(LALT(Key_LeftGui))
@@ -39,20 +40,20 @@ enum {
   MACRO_VERSION_INFO
 };
 
-#define Key_Exclamation LSHIFT(Key_1)
-#define Key_At          LSHIFT(Key_2)
-#define Key_Hash        LSHIFT(Key_3)
-#define Key_Dollar      LSHIFT(Key_4)
-#define Key_Percent     LSHIFT(Key_5)
-#define Key_Caret       LSHIFT(Key_6)
-#define Key_And         LSHIFT(Key_7)
-#define Key_Star        LSHIFT(Key_8)
-#define Key_Plus        LSHIFT(Key_Equals)
+#define Key_Bang    LSHIFT(Key_1)
+#define Key_At      LSHIFT(Key_2)
+#define Key_Hash    LSHIFT(Key_3)
+#define Key_Dollar  LSHIFT(Key_4)
+#define Key_Percent LSHIFT(Key_5)
+#define Key_Caret   LSHIFT(Key_6)
+#define Key_And     LSHIFT(Key_7)
+#define Key_Star    LSHIFT(Key_8)
+#define Key_Plus    LSHIFT(Key_Equals)
 
 enum {
   QWERTY,
   FUN,
-  UPPER
+  NUMB
 };
 
 enum { TAPDANCE_LEFT_BRACKET,
@@ -66,18 +67,18 @@ KEYMAPS(
         //
         // left hand
         //
-        Key_Q,   Key_W,         Key_E,            Key_R,        Key_T,
-        Key_A,   SFT_T(S),      CTL_T(D),         GUI_T(F),     ALT_T(G),
-        Key_Z,   Key_X,         Key_C,            Key_V,        Key_B,        SFT_T(Tab),
-        MO(FUN), Key_Escape,    Key_Backspace,    Key_Space,    KEYCHORD_MEH, TD(TAPDANCE_LEFT_BRACKET),
+        Key_Q,                Key_W,            Key_E,            Key_R,            Key_T,        /*-----*/
+        SFT_T(A),             CTL_T(S),         GUI_T(D),         ALT_T(F),         Key_G,        /*-----*/
+        Key_Z,                Key_X,            Key_C,            Key_V,            Key_B,        SFT_T(Tab),
+        LT(FUN,Backtick),     Key_Escape,       Key_Backspace,    Key_Space,        KEYCHORD_MEH, TD(TAPDANCE_LEFT_BRACKET),
 
         //
         // right hand
         //
         /*-----*/                   Key_Y,             Key_U,        Key_I,            Key_O,         Key_P,
-        /*-----*/                   ALT_T(H),          GUI_T(J),     CTL_T(K),         SFT_T(L),      Key_Semicolon,
+        /*-----*/                   Key_H,             ALT_T(J),     GUI_T(K),         CTL_T(L),      SFT_T(Semicolon),
         Key_Enter,                  Key_N,             Key_M,        Key_Comma,        Key_Period,    Key_Slash,
-        TD(TAPDANCE_RIGHT_BRACKET), KEYCHORD_HYPER,    Key_Space,    Key_Backslash,    Key_Minus,     MO(UPPER)
+        TD(TAPDANCE_RIGHT_BRACKET), KEYCHORD_HYPER,    Key_Space,    Key_Backslash,    Key_Minus,     L(NUMB)
     ),
 
     [FUN] = KEYMAP_STACKED
@@ -85,37 +86,37 @@ KEYMAPS(
         //
         // left hand
         //
-        Key_F1, Key_F2,  Key_F3,  Key_F4,        ___,
-        Key_F5, Key_F6,  Key_F7,  Key_F8,        ___,
-        Key_F9, Key_F19, Key_F11, Key_F12,       ___, ___,
-        ___,    ___,     ___,     Key_Backspace, ___, ___,
+        Key_F1,   Key_F2,     Key_F3,  Key_F4,        Key_F5, /*-----*/
+        ___,      ___,        ___,     ___,           ___,    /*-----*/
+        ___,      ___,        ___,     ___,           ___,    ___,
+        ___,      ___,        ___,     Key_Backspace, ___,    ___,
 
         //
         // right hand
         //
-        /*-----*/ Consumer_Mute,            ___,                        Key_UpArrow,             Key_LeftShift,          Key_LeftControl,
-        /*-----*/ Consumer_VolumeIncrement, Key_LeftArrow,              Key_DownArrow,           Key_RightArrow,         Key_LeftGui,
-        ___,      Consumer_VolumeDecrement, Consumer_ScanPreviousTrack, Consumer_PlaySlashPause, Consumer_ScanNextTrack, Key_LeftAlt,
-        ___,      ___,                      Key_Backspace,              ___,                     ___,                    ___
+        /*-----*/ Key_F6,                                        Key_F7,                     Key_F8,                     Key_F9,                  Key_F10,
+        /*-----*/ Consumer_Mute,                                 ___,                        Key_UpArrow,                Key_LeftShift,           Key_LeftGui,
+        ___,      Consumer_VolumeIncrement,                      Key_LeftArrow,              Key_DownArrow,              Key_RightArrow,          Key_LeftAlt,
+        ___,      Consumer_VolumeDecrement,                      Key_Backspace,              Consumer_ScanPreviousTrack, Consumer_PlaySlashPause, Consumer_ScanNextTrack
     ),
 
-    [UPPER] = KEYMAP_STACKED
+    [NUMB] = KEYMAP_STACKED
     (
         //
         // left hand
         //
-        ___,          Key_And,         Key_Star,    Key_Plus,    ___,
-        Key_PageUp,   Key_Dollar,      Key_Percent, Key_Caret,   ___,
-        Key_PageDown, Key_Exclamation, Key_At,      Key_Hash,    ___, ___,
-        ___,          ___,             ___,         ___,         ___, ___,
+        Key_1,                 Key_2,           Key_3,       Key_4,       Key_5,        /*-----*/
+        Key_Bang,              Key_At,          Key_Hash,    Key_Dollar,  Key_Percent,  /*-----*/
+        ___,                   ___,             ___,         ___,         ___,          Key_PageUp,
+        L(QWERTY),             ___,             ___,         ___,         ___,          Key_PageDown,
 
         //
         // right hand
         //
-        /*-----*/ ___,  Key_7, Key_8, Key_9, ___,
-        /*-----*/ ___,  Key_4, Key_5, Key_6, Key_Home,
-        ___,      ___,  Key_1, Key_2, Key_3, Key_End,
-        ___,      ___,  ___,   Key_0, ___,   ___
+        /*-----*/        Key_6,      Key_7,   Key_8,    Key_9,    Key_0,
+        /*-----*/        Key_Caret,  Key_And, Key_Star, TOPSY(9), TOPSY(0),
+        Key_Home,        ___,        ___,     ___,      ___,      ___,
+        Key_End,         ___,        ___,     ___,      ___,      L(QWERTY)
     )
 )
 /* *INDENT-ON* */
